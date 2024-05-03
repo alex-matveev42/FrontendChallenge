eb57120a3c8eafbfd9dc5c741eaa2e7d3ccdc9b2
Alex Matveev <133750432+alex-matveev42@users.noreply.github.com>
2024-05-03 21:03:14 +0200

    add logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { MatSelectModule } from '@angular/material/select'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatDatepicker, MatDatepickerModule } from '@angular/material/datepicker'
+import { MatNativeDateModule } from '@angular/material/core'
+import { provideMomentDateAdapter } from '@angular/material-moment-adapter'
+import { SWEDEN_FORMAT } from './app.config'
+import { FormsModule } from '@angular/forms'
+import moment from 'moment'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [provideMomentDateAdapter(SWEDEN_FORMAT)],
 })
 export class AppComponent {
-  title = 'FrontendChallenge'
+  timeTo = 'Time to';
+  title: string | null = null;
+  selectedDate: moment.Moment | null = null;
+  countdown: string = '';
+
+  @ViewChild('datepicker') datepicker!: MatDatepicker<Date>
+
+   futureDateFilter = (d: Date | null): boolean => {
+    const today = new Date();
+    return !d || d >= today;
+  }
+
+  openDatepicker() {
+    this.datepicker.open()
+  }
+
+  onDatepickerClosed(){
+    if(this.selectedDate){
+      this.startCountdown();
+    }
+  }
+
+  startCountdown() {
+    setInterval(() => {
+      const now = moment();
+      if (!this.selectedDate) return;
+
+      const diff = moment.duration(this.selectedDate.diff(now));
+
+      const days = diff.days();
+      const hours = diff.hours();
+      const minutes = diff.minutes();
+      const seconds = diff.seconds();
+
+      this.countdown = `${days} days, ${hours} h, ${minutes} m, ${seconds} s`;
+    }, 1000);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
 import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes'
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), provideAnimationsAsync()],
+}
+
+export const SWEDEN_FORMAT = {
+  parse: {
+    dateInput: 'YYYY-MM-DD',
+  },
+  display: {
+    dateInput: 'YYYY-MM-DD',
+    monthYearLabel: 'YYYY',
+    dateA11yLabel: 'YYYY-MM-DD',
+    monthYearA11yLabel: 'YYYY',
+  },
 }
